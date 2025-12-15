684d4a0685c169fd1fe108421f4fa169d95dd4d5
aamir-hassan-15 <aamirhassanlone3@gmail.com>
2025-12-15 15:29:06 +0530

    added some code

---
+++ src/components/Weather.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Weather.css'
+
+
+ export default function  Weather  ()  {
+  return (
+    <div>
+        <h1>Weather</h1>
+      
+    </div>
+  )
+}
+
+// export default Weather
